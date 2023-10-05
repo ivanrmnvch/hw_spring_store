63366948596028655dde410f8407bcc509fa7db5
ivanrmnvch <ivansylkin@gmail.com>
2023-10-05 22:53:52 +0300

    init

---
+++ src/main/java/com/example/hw_spring_store/HwSpringStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.hw_spring_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwSpringStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwSpringStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hw_spring_store/HwSpringStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hw_spring_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwSpringStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
